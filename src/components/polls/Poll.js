566882176fc73c02a90091fbb739c4e397b790da
kejbi <biegajski.kacper@gmail.com>
2020-05-04 12:22:34 +0200

    fixed problem with changing vote in poll

--- src/components/polls/Poll.js
+++ src/components/polls/Poll.js
@@ -6,42 +6,43 @@ import classnames from "classnames";
 import { BASE_URL } from "../../properties/consts";
 import axios from "axios";
 
-const Poll = props => {
+const Poll = (props) => {
   const [poll, setPoll] = useState(props.poll);
   const [loading, setLoading] = useState(false);
   const [userState, dispatch] = useContext(UserContext);
 
   let config = {
     headers: {
-      Authorization: "Bearer " + userState.user.token
-    }
+      Authorization: "Bearer " + userState.user.token,
+    },
   };
 
-  const handleVote = event => {
+  const handleVote = (event) => {
     setLoading(true);
     axios
       .put(`${BASE_URL}/poll/`, { answerId: event.target.value }, config)
-      .then(response => {
+      .then((response) => {
         dispatch({
           type: "MESSAGE",
-          payload: { message: "Pomyślnie oddano głos", type: "success" }
+          payload: { message: "Pomyślnie oddano głos", type: "success" },
         });
+        console.log(response.data);
         setPoll(response.data);
         setLoading(false);
       })
-      .catch(error => {
+      .catch((error) => {
         setLoading(false);
         if (error.response === undefined) {
           dispatch({
             type: "MESSAGE",
-            payload: { message: "Błąd serwera", type: "danger" }
+            payload: { message: "Błąd serwera", type: "danger" },
           });
         } else if (error.response.data.status === 401) {
           dispatch({ type: "TIMEOUT" });
         } else {
           dispatch({
             type: "MESSAGE",
-            payload: { message: "Coś poszło nie tak", type: "danger" }
+            payload: { message: "Coś poszło nie tak", type: "danger" },
           });
         }
       });
@@ -49,7 +50,8 @@ const Poll = props => {
 
   useEffect(() => {
     setPoll(props.poll);
-  }, [props]);
+  }, [props.poll]);
+
   return loading ? (
     <Spinner className='loading-spinner' color='primary' />
   ) : (
@@ -58,7 +60,7 @@ const Poll = props => {
       <h5>Data rozpoczęcia: {poll.startDate}</h5>
       <h5>Data zakończenia: {poll.finishDate}</h5>
       <h6>Zagłosowało: {poll.totalVotes}</h6>
-      {poll.answers.map(answer => {
+      {poll.answers.map((answer) => {
         return (
           <div>
             <div className='text-center'>{answer.answer}</div>
